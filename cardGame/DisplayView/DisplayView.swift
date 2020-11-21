3d60a848a7ad6ebaeaea907f119d5d73d8adf17c
JuanJCO <jcarroolmedo@gmail.com>
2020-11-21 21:16:02 +0100

    Code Optimization / Added Functions /

--- cardGame/DisplayView/DisplayView.swift
+++ cardGame/DisplayView/DisplayView.swift
@@ -13,8 +13,6 @@ class UIDisplayView: UIView{
     @IBOutlet weak var view: UIView!
     @IBOutlet weak var label: UILabel!
     
-    let relativeFont: CGFloat = 0.15
-    
     override init(frame: CGRect) {
         super.init (frame: frame)
         initView()
@@ -31,7 +29,6 @@ class UIDisplayView: UIView{
         let nib = UINib(nibName: "DisplayView", bundle: nil)
         nib.instantiate(withOwner: self, options: nil)
         self.view.frame = bounds
-        //label.font = label.font.withSize(self.view.frame.height * relativeFont)
         addSubview(view)
         
     }
--- cardGame/ViewController.swift
+++ cardGame/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    // MARK: Outlets & Arrays
+    // MARK: Outlets & Variables
     
     @IBOutlet weak var leftBtn: UIButton!
     @IBOutlet weak var rightBtn: UIButton!
@@ -21,12 +21,12 @@ class ViewController: UIViewController {
     var arrayNumberNegative = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11]
     var arrayNumberPositive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     
+    var sum: Int = 0
+    
     //MARK: ViewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         
         displayLabel = display.label
 
@@ -55,85 +55,87 @@ class ViewController: UIViewController {
     
     // MARK: UI Functions
     
-    //NEGATIVE NUMBER FUNCTION LOGIC
-    
-    @IBAction func leftBtnAction(_ sender: Any) {
-            
-        //Parseo a Int el .text del Label negativo y el del contador
+    //Buttons Action Logic
+    @IBAction func btnAction (_ sender: UIButton){
+        
+        //Parseo a Int y almaceno el contenido de los 3 labels
         let countLabelNumber = Int(displayLabel.text!)
-        let leftLabelNumber = Int(leftBtn.titleLabel!.text!)!
+        let negativeLabelNumber = Int(leftBtn.titleLabel!.text!)!
+        let positiveLabelNumber = Int(rightBtn.titleLabel!.text!)!
         
-        //Almaceno el cálculo de ambos números y lo muestro en el label del contador
-        let sum = countLabelNumber! + leftLabelNumber
-        displayLabel.text = String(sum)
-        
-        //Compruebo si el array de números sigue teniendo contenido
-        //Si aún no está vacío, recojo una posición aleatoria y la muestro en el label
-        if (!arrayNumberNegative.isEmpty){
-            let randomNumberNegative = arrayNumberNegative.randomElement()
-            leftBtn.setTitle(String(randomNumberNegative!), for: .normal)
+        //Switch para dividir el IBAction (Case 0 = Negativo / Case 1 = Positivo)
+        switch sender.tag {
+
+        //Negativos
+        case 0:
+            //Almaceno el cálculo de ambos números y lo muestro en el label de la bomba
+            self.sum = countLabelNumber! + negativeLabelNumber
             
-            for (i, v) in arrayNumberNegative.enumerated(){
-                if (v == randomNumberNegative){
-                    arrayNumberNegative.remove(at: i)
+            addZerosToLabelNumber()
+            
+            //Compruebo si el array de números sigue teniendo contenido
+            //Si aún no está vacío, recojo una posición aleatoria y la muestro en el label
+            if (!arrayNumberNegative.isEmpty){
+                let randomNumberNegative = arrayNumberNegative.randomElement()
+                leftBtn.setTitle(String(randomNumberNegative!), for: .normal)
+                
+                for (i, v) in arrayNumberNegative.enumerated(){
+                    if (v == randomNumberNegative){
+                        arrayNumberNegative.remove(at: i)
+                    }
                 }
+                //Si el array está vacío, le asigno el número 0, de manera que su interacción no tenga ningún efecto
+            } else {
+                leftBtn.setTitle("0", for: .normal)
             }
-            //Si el array está vacío, le asigno el número 0, de manera que su interacción no tenga ningún efecto
-        } else {
-            leftBtn.setTitle("0", for: .normal)
+        
+        //Positivos
+        case 1:
+            self.sum = countLabelNumber! + positiveLabelNumber
+            
+            addZerosToLabelNumber()
+            
+            if (!arrayNumberPositive.isEmpty){
+                let randomNumberPositive = arrayNumberPositive.randomElement()
+                rightBtn.setTitle(String(randomNumberPositive!), for: .normal)
+                
+                for (i,v) in arrayNumberPositive.enumerated(){
+                    if (v == randomNumberPositive){
+                        arrayNumberPositive.remove(at: i)
+                    }
+                }
+            } else {
+                rightBtn.setTitle("0", for: .normal)
+            }
+            
+        default:
+            displayLabel.text = "000"
         }
         
-        //Cambio el color del texto del contador, para indicar visualmente lo cerca/lejos que estás del número 21
-        if (Int(displayLabel.text!)! < 10){
-            displayLabel.textColor = UIColor.red
-        }
-        if (Int(displayLabel.text!)! > 9 && Int(displayLabel.text!)! < 19){
-            displayLabel.textColor = UIColor.yellow
-        }
-        if (Int(displayLabel.text!)! > 18 && Int(displayLabel.text!)! < 21){
-            displayLabel.textColor = UIColor.blue
-        }
-        if (Int(displayLabel.text!)! == 21){
-            displayLabel.textColor = UIColor.green
-        }
-        
+        changeLabelTextColor()
+
     }
     
-    //POSITIVE NUMBER FUNCTION LOGIC
-    
-    //Su explicación es la misma que en la anterior función
-    @IBAction func rightBtnAction(_ sender: Any) {
+    //Cambio el color del label de la bomba, según se acerque al número 21
+    func changeLabelTextColor(){
         
-        let countLabelNumber = Int(displayLabel.text!)
-        let rightLabelNumber = Int(rightBtn.titleLabel!.text!)!
-        
-        let sum = countLabelNumber! + rightLabelNumber
-        displayLabel.text = String(sum)
-        
-        if (!arrayNumberPositive.isEmpty){
-            let randomNumberPositive = arrayNumberPositive.randomElement()
-            rightBtn.setTitle(String(randomNumberPositive!), for: .normal)
-            
-            for (i,v) in arrayNumberPositive.enumerated(){
-                if (v == randomNumberPositive){
-                    arrayNumberPositive.remove(at: i)
-                }
-            }
-        } else {
-            rightBtn.setTitle("0", for: .normal)
-        }
-        
-        if (Int(displayLabel.text!)! < 10){
-            displayLabel.textColor = UIColor.red
-        }
         if (Int(displayLabel.text!)! > 9 && Int(displayLabel.text!)! < 19){
             displayLabel.textColor = UIColor.yellow
-        }
-        if (Int(displayLabel.text!)! > 18 && Int(displayLabel.text!)! < 21){
+        }else if (Int(displayLabel.text!)! > 18 && Int(displayLabel.text!)! < 21){
             displayLabel.textColor = UIColor.blue
-        }
-        if (Int(displayLabel.text!)! == 21){
+        }else if (Int(displayLabel.text!)! == 21){
             displayLabel.textColor = UIColor.green
+        }else {
+            displayLabel.textColor = UIColor.red
+        }
+    }
+    
+    //Añade 0s al text del Label en función del número que haya
+    func addZerosToLabelNumber(){
+        if (sum > 9){
+            displayLabel.text = "0" + String(sum)
+        }else{
+            displayLabel.text = "00" + String(sum)
         }
     }
 }
